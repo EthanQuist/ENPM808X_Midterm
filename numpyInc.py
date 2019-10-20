0f6a8119265998dc551731b45004b11bc978236d
Corbyn Yhap <cyhap12@gmail.com>
2019-10-20 15:03:03 -0400

    Found typo in python script. (Task 2-9)

--- numpyInc.py
+++ numpyInc.py
@@ -2,6 +2,6 @@ import numpy
 import os
 numpyPath = numpy.get_include()
 print(numpyPath)
-command = "ln -s " +  numpyPath + " /usr/include/numpy"
+command = "sudo ln -s " +  numpyPath + " /usr/include/numpy"
 print(command)
-os.sys(command)
+os.system(command)
